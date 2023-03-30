bfc58ff7e2d0a53f399641391d2bc1149fac2e09
Riaz <riazbls455@gmail.com>
2023-03-31 01:15:39 +0530

    Added OneToMany

---
+++ JpaMapping/src/main/java/com/example/jpamapping/inheritance/entity/Veichle.java
@@ -0,0 +1,5 @@
+package com.example.jpamapping.inheritance.entity;
+
+public abstract class Veichle {
+
+}
--- JpaMapping/src/main/java/com/example/jpamapping/oneToOne/services/UserService.java
+++ JpaMapping/src/main/java/com/example/jpamapping/oneToOne/services/UserService.java
@@ -3,8 +3,11 @@ package com.example.jpamapping.oneToOne.services;
 import com.example.jpamapping.oneToOne.entities.User;
 import com.example.jpamapping.oneToOne.repository.UserRepository;
 import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -13,13 +16,19 @@ import org.springframework.stereotype.Service;
 public class UserService {
   @Autowired
   UserRepository userRepository;
+  Logger logger = LoggerFactory.getLogger(UserService.class);
 
   @Cacheable(cacheNames = "user")
   public User getUser(Long id) {
       Optional<User> optionalUser = userRepository.findById(id);
       if(optionalUser.isPresent()){
+          logger.info("fnwkfw efhweoifw {}",optionalUser.get().getProfile().getId());
           return optionalUser.get();
       }
       return optionalUser.orElseThrow();
+  }
+  @CacheEvict(cacheNames = "user",allEntries = true)
+    public void save(User user){
+      userRepository.save(user);
   }
 }
