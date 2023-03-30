b74a183e6943d93a56db9e269ab0bfe1e0684723
Riaz <riazbls455@gmail.com>
2023-03-31 01:15:15 +0530

    Added OneToMany

--- JpaMapping/src/main/java/com/example/jpamapping/oneToMany/services/AuthorService.java
+++ JpaMapping/src/main/java/com/example/jpamapping/oneToMany/services/AuthorService.java
@@ -4,21 +4,15 @@ import com.example.jpamapping.oneToMany.entities.Author;
 import com.example.jpamapping.oneToMany.repository.AuthorRepository;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheConfig;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@CacheConfig(cacheNames = "authorCache")
-@Transactional
+//@CacheConfig(cacheNames = "authorCache")
 public class AuthorService {
     @Autowired
     AuthorRepository authorRepository;
 
-  @Cacheable(cacheNames = "authors")
-  @Transactional
+//  @Cacheable(cacheNames = "authors")
   public Author getAuthor(Long id) {
         Optional<Author> optionalAuthor = authorRepository.findById(id);
         if(optionalAuthor.isPresent()){
@@ -27,8 +21,8 @@ public class AuthorService {
         return optionalAuthor.orElseThrow();
     }
 
-  @CacheEvict(cacheNames = "authors",allEntries = true)
-  @Transactional
+//  @CacheEvict(cacheNames = "authors",allEntries = true)
+
   public boolean saveAuthor(Author author) {
         authorRepository.save(author);
         return true;
