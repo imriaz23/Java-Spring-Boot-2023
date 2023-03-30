1ea28f16f5ea73bf559e143008648d5d3a5f80e7
Riaz <riazbls455@gmail.com>
2023-03-31 01:15:28 +0530

    Added OneToMany

---
+++ JpaMapping/src/main/java/com/example/jpamapping/manyToMany/MAuthorCommandLineRunner.java
@@ -0,0 +1,46 @@
+package com.example.jpamapping.manyToMany;
+
+import com.example.jpamapping.manyToMany.entity.MAuthor;
+import com.example.jpamapping.manyToMany.entity.MBook;
+import com.example.jpamapping.manyToMany.service.MAuthorService;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MAuthorCommandLineRunner implements CommandLineRunner {
+  @Autowired MAuthorService mAuthorService;
+  @Override
+  public void run(String... args) throws Exception {
+        MAuthor author1 = new MAuthor();
+        author1.setName(RandomStringUtils.randomAlphabetic(6,8));
+        MAuthor author2 = new MAuthor();
+        author2.setName(RandomStringUtils.randomAlphabetic(6,8));
+
+        MBook book1 = new MBook();
+        book1.getAuthors().add(author1);
+        book1.getAuthors().add(author2);
+        author1.getBooks().add(book1);
+        author2.getBooks().add(book1);
+
+        MBook book2 = new MBook();
+        book2.getAuthors().add(author1);
+        author1.getBooks().add(book2);
+
+        MBook book3 = new MBook();
+        book3.getAuthors().add(author2);
+        author2.getBooks().add(book3);
+
+        MBook book4 = new MBook();
+        book4.getAuthors().add(author1);
+        author1.getBooks().add(book4);
+        book1.setTitle(RandomStringUtils.randomAlphanumeric(9,18));
+      book2.setTitle(RandomStringUtils.randomAlphanumeric(9,18));
+      book3.setTitle(RandomStringUtils.randomAlphanumeric(9,18));
+      book4.setTitle(RandomStringUtils.randomAlphanumeric(9,18));
+
+    mAuthorService.createAuthor(author1);
+    mAuthorService.createAuthor(author2);
+
+    }}
---
+++ JpaMapping/src/main/java/com/example/jpamapping/manyToMany/entity/MAuthor.java
@@ -0,0 +1,22 @@
+package com.example.jpamapping.manyToMany.entity;
+
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Data;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+
+@Entity
+@Data
+public class MAuthor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column private String name;
+
+  @ManyToMany(mappedBy = "authors", fetch = FetchType.EAGER)
+  @Cascade({CascadeType.SAVE_UPDATE, CascadeType.MERGE, CascadeType.PERSIST})
+//  @JsonManagedReference
+  private List<MBook> books = new ArrayList<>();
+}
