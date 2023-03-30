328951ac1680af9e95f7aae45018288ca9abdf51
Riaz <riazbls455@gmail.com>
2023-03-31 01:16:25 +0530

    added many to many mapping

--- JpaMapping/src/main/java/com/example/jpamapping/oneToOne/config/RedisConfig.java
+++ JpaMapping/src/main/java/com/example/jpamapping/oneToOne/config/RedisConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
@@ -20,7 +21,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @AutoConfigureAfter(RedisAutoConfiguration.class)
-//@EnableCaching
+@EnableCaching
 public class RedisConfig {
   @Autowired @Lazy private CacheManager cacheManager;
 
